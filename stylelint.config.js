18c08172287a7798f9943862a1af455f718350a8
empitsu <yoshie8080@gmail.com>
2019-12-15 23:38:25 +0900

    feat: configure stylelint for scss

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
+  extends: 'stylelint-config-recommended',
+  plugins: ['stylelint-scss']
 }
