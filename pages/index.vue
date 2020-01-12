fcb6f82aaaefbf93c8815cdffd99126f1362e4b1
empitsu <yoshie8080@gmail.com>
2020-01-13 01:40:14 +0900

    feat: implement fetching portfolio data

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,29 @@
 <template>
-  <VMainTemplate />
+  <VMainTemplate :portfolio-data="portfolioData" />
 </template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import { Context } from '@nuxt/types'
+import VMainTemplate, {
+  PortfolioData
+} from '~/components/templates/VMainTemplate/index.vue'
 
-<script>
-import VMainTemplate from '~/components/templates/VMainTemplate/index.vue'
-
-export default {
+@Component({
   components: {
     VMainTemplate
+  },
+  async asyncData(
+    ctx: Context
+  ): Promise<{
+    portfolioData: PortfolioData
+  }> {
+    const result = await ctx.$axios.$get('http://localhost:8080/portfolio-data')
+    return {
+      portfolioData: result
+    }
   }
+})
+export default class extends Vue {
+  portfolioData = {} as PortfolioData
 }
 </script>
